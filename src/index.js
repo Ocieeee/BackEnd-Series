0ca5576076c32c6d31a6caf828df6aff264ec6cb
Om chaudhari <omchaudhari0003@gmail.com>
2024-03-29 14:23:41 +0530

    Connect mongoDB Server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_DB_URI} / ${DB_NAME}`);
+  } catch (error) {
+    console.error("ERROR :", error);
+    throw err;
+  }
+})();*/
